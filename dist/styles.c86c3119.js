725e503d1e76d6491e6d43162f2830d5abfdde15
unknown <nbageja@gmail.com>
2020-11-12 21:21:27 +0530

    Added transition

--- dist/styles.c86c3119.js
+++ dist/styles.c86c3119.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52197" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55730" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
